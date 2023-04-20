1811b63e73181dc39394677d854197ea4bcfdddc
Jake Burton <jakbu@netcompany.com>
2023-04-20 16:52:46 +0200

    Start Algs

---
+++ src/RoutePlanning.Domain/Locations/Services/ICheapestPathService.cs
@@ -0,0 +1,8 @@
+﻿using Netcompany.Net.DomainDrivenDesign.Services;
+
+namespace RoutePlanning.Domain.Locations.Services;
+
+public interface ICheapestPathService : IDomainService
+{
+    int CalculateCheapestPath(Location source, Location target);
+}
