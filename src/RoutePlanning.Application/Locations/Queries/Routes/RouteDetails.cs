1420c7c963db41e2749eb9babe931d8e3caaa601
nthuy <thuynguyen.webdev@gmail.com>
2023-04-20 23:58:09 +0200

    add controller

---
+++ src/RoutePlanning.Application/Locations/Queries/Routes/RouteDetails.cs
@@ -0,0 +1,8 @@
+﻿namespace RoutePlanning.Application.Locations.Queries.Routes;
+
+public record RouteDetails
+{
+    public int Cost { get; set; }
+
+    public int Time { get; set; }
+}
---
+++ src/RoutePlanning.Application/Locations/Queries/Routes/RouteDetailsQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using Netcompany.Net.Cqs.Queries;
+using RoutePlanning.Domain.Locations.Services;
+using RoutePlanning.Domain.Locations;
+using Microsoft.EntityFrameworkCore;
+
+namespace RoutePlanning.Application.Locations.Queries.Routes;
+public class RouteDetailsQueryHandler : IQueryHandler<RouteDetailsQuery, int>
+{
+    private readonly IQueryable<Location> _locations;
+    private readonly IShortestDistanceService _shortestDistanceService;
+
+    public RouteDetailsQueryHandler(IQueryable<Location> locations, IShortestDistanceService shortestDistanceService)
+    {
+        _locations = locations;
+        _shortestDistanceService = shortestDistanceService;
+    }
+
+    public async Task<int> Handle(RouteDetailsQuery request, CancellationToken cancellationToken)
+    {
+        var source = await _locations.FirstAsync(l => l.Id == request.SourceId, cancellationToken);
+        var destination = await _locations.FirstAsync(l => l.Id == request.DestinationId, cancellationToken);
+
+        var distance = _shortestDistanceService.CalculateShortestDistance(source, destination);
+
+        return distance;
+    }
+}
