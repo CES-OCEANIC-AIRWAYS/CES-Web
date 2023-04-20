988195306142734d2e9162a1982536d47b61c414
s3822042 <s3822042@rmit.edu.vn>
2023-04-20 20:39:03 +0200

    update ui

--- src/RoutePlanning.Client.Web/Pages/Login.razor.cs
+++ src/RoutePlanning.Client.Web/Pages/Login.razor.cs
@@ -1,10 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using MediatR;
+using Microsoft.AspNetCore.Components;
+using RoutePlanning.Application.Users.Queries.AuthenticatedUser;
+using RoutePlanning.Client.Web.Authentication;
 
 namespace RoutePlanning.Client.Web.Pages;
 
-public class Login_razor : PageModel
+public sealed partial class Login
 {
-    public void OnGet()
+    private string Username { get; set; } = string.Empty;
+    private string Password { get; set; } = string.Empty;
+    private AuthenticatedUser? User { get; set; }
+    private bool ShowAuthError { get; set; }
+
+    [Inject] private SimpleAuthenticationStateProvider AuthStateProvider { get; set; } = default!;
+
+    [Inject] private IMediator Mediator { get; set; } = default!;
+
+    private async Task Signin()
     {
+        User = await Mediator.Send(new AuthenticatedUserQuery(Username, Password), CancellationToken.None);
+
+        ShowAuthError = User is null;
+
+        if (User is not null)
+        {
+            await AuthStateProvider.SetAuthenticationStateAsync(new UserSession(User.Username));
+        }
+    }
+
+    private async Task Logout()
+    {
+        await AuthStateProvider.ClearAuthenticationStateAsync();
+
+        ShowAuthError = false;
     }
 }
