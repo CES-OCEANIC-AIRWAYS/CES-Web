99d6bbce66cd2781050762eab8b130fa8c86c77c
vtlua <vtlua@netcompany.com>
2023-04-21 14:53:29 +0700

    fe integration

---
+++ src/RoutePlanning.Client.Web/Pages/PackageInformation.razor.cs
@@ -0,0 +1,20 @@
+﻿
+using MediatR;
+using Microsoft.AspNetCore.Components;
+using RoutePlanning.Application.Locations.Queries.SelectableLocationList;
+namespace RoutePlanning.Client.Web.Pages;
+
+public partial class PackageInformation
+{
+    private List<SelectableLocation> locations = new List<SelectableLocation>();
+
+    [Inject]
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    private IMediator mediator { get; set; }
+    protected override async Task OnInitializedAsync()
+    {
+        var query = new SelectableLocationListQuery();
+        var data = await mediator.Send(query);
+        locations = data.ToList();
+    }
+}
